0c7723b09f58311192111b4cac840710603045df
jcazorla2000 <jose.cazorla00@gmail.com>
2019-11-26 18:05:28 -0600

    Actualizacion del mapa location, vistas y proteccion de rutas

--- HostInn/middlewares/auth.middlewares.js
+++ HostInn/middlewares/auth.middlewares.js
@@ -1,15 +1,46 @@
 exports.isLoggedIn = (req, res, next) => {
   req.isAuthenticated() ? next() : res.redirect("/login");
 };
-
 exports.isNotLoggedIn = (req, res, next) => {
-  !req.isAuthenticated() ? next() : res.redirect("/profile");
+  !req.isAuthenticated() ? next() : res.redirect("./places/show");
 };
-
 exports.checkRole = role => (req, res, next) => {
   if (req.user.role === role) {
     return next();
   } else {
     return res.send("No te confundas 💁‍♀️");
   }
+};
+exports.whichRole = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    if (req.user.role === "Huesped") {
+      req.app.locals.isHuesped = true;
+    } else if (req.user.role === "Anfitrion") {
+      req.app.locals.isAnfitrion = true;
+    } else {
+      req.app.locals.isHuesped = false;
+      req.app.locals.isAnfitrion = false;
+    }
+  } else {
+    req.app.locals.isHuesped = false;
+    req.app.locals.isAnfitrion = false;
+  }
+  next();
+};
+// Middleware to ensure the login
+exports.ensureLogin = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    return res.redirect('/auth/login');
+  }
+}
+
+exports.checkUser = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    req.app.locals.logged = true;
+  } else {
+    req.app.locals.logged = false;
+  }
+  return next();
 };
--- HostInn/routes/index.js
+++ HostInn/routes/index.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const router  = express.Router();
 const Place = require("../models/Place")
+const { ensureLogin, whichRole, checkUser} = require("../middlewares/auth.middlewares")
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get('/', checkUser, (req, res, next) => {
   res.render('index');
 });
 
-router.get("/map/locations", async (req,res, next) => {
+router.get("/map/locations", checkUser, async (req,res, next) => {
   places = await Place.find()
-  res.render("map/locations", {places})
+  res.render("map/locations", {places} )
   console.log(places)
 })
+router.get("/places/create", checkUser, ensureLogin, (req,res, next) => {
+  res.render("places/create",)
+})
 
+router.get("/places/show", checkUser, ensureLogin, (req,res,next)=>{
+  res.render("places/show")
+})
+
+router.get("/places/delete", checkUser, ensureLogin, (req,res,next)=>{
+  res.render("places/delete")
+})
+
+router.get("/places/edit", checkUser, ensureLogin, (req,res,next)=>{
+  res.render("places/edit")
+})
 module.exports = router;
